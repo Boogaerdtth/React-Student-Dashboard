e19d87203451dee8012093417d523d7b0bbd5e2a
Jasper Boogaerdt t Hooft <jasper17@live.nl>
2020-12-23 08:48:43 +0100

    add the first graph of student

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,12 +5,69 @@ import Bargraph from './graphs/Bargraph'
 import Home from "./Home"
 import Evelyn from './students/Evelyn'
 import Aranka from './students/Aranka'
+
 const studentData = require('./Studentdata.json')
 
-// const handleChange = event => {
-//     const { name, value } = event.target
-//     this.setState({ [name]: value })
-// }
+
+const assignmentRatingAverage = [
+    { assignment: "SCRUM" },
+    { assignment: "W1D1-1" },
+    { assignment: "W1D2-1" },
+    { assignment: "W1D2-2" },
+    { assignment: "W1D2-3" },
+    { assignment: "W1D2-4" },
+    { assignment: "W1D2-5" },
+    { assignment: "W1D3-1" },
+    { assignment: "W1D3-2" },
+    { assignment: "W1D3-4" },
+    { assignment: "W1D3-5" },
+    { assignment: "W1D3 - Project - Guess the number" },
+    { assignment: "W1D4-1" },
+    { assignment: "W1D4 - Project - Kleurentoggle" },
+    { assignment: "W1D5 - Project - Galgje" },
+    { assignment: "W2D1-1" },
+    { assignment: "W2D1-2" },
+    { assignment: "W2D2-1" },
+    { assignment: "W2D2-2" },
+    { assignment: "W2D2-3" },
+    { assignment: "W2D3-1" },
+    { assignment: "W2D3-2" },
+    { assignment: "W2D3-3" },
+    { assignment: "W2D4-1" },
+    { assignment: "W2D4-2" },
+    { assignment: "W2D4-3" },
+    { assignment: "W2D5 - Project - Filmzoeker" },
+    { assignment: "W3D1-1" },
+    { assignment: "W3D1-2" },
+    { assignment: "W3D1-3" },
+    { assignment: "W3D1-4" },
+    { assignment: "W3D2-1" },
+    { assignment: "W3D2-2" },
+    { assignment: "W3D2-3" },
+    { assignment: "W3D3-1" },
+    { assignment: "W3D3-2" },
+    { assignment: "W3D3-3" },
+    { assignment: "W3D3-4" },
+    { assignment: "W3D4-1" },
+    { assignment: "W3D4-2" },
+    { assignment: "W3D5 - Project - Todo-list" },
+    { assignment: "W4D2-1" },
+    { assignment: "W4D2-2" },
+    { assignment: "W4D2-3" },
+    { assignment: "W4D2-4" },
+    { assignment: "W4D3-1" },
+    { assignment: "W4D3-2" },
+    { assignment: "W4D3-3" },
+    { assignment: "W4D3-4" },
+    { assignment: "W4D3-5" },
+    { assignment: "W4D3 - Project - Next-level CSS" },
+    { assignment: "W5D4-1" },
+    { assignment: "W5D5 - Project - Lil_Playlist" },
+    { assignment: "W6D1-1" },
+    { assignment: "W6D2-1" },
+    { assignment: "W6D2 - Project - Eindopdracht" },
+];
+
 const Container = () => {
 
     // console.log(studentData.student[0].assignments[0])
@@ -21,16 +78,24 @@ const Container = () => {
                     <ChartExample />
                 </Route>
                 <Route path="/bargraph">
-                    <Bargraph studentData={studentData} />
+                    <Bargraph
+                        studentData={studentData}
+                        assignmentRatingAverage={assignmentRatingAverage}
+                    />
                 </Route>
                 <Route path="/home">
                     <Home />
                 </Route>
                 <Route path="/students/evelyn">
-                    <Evelyn />
+                    <Evelyn
+                        studentData={studentData}
+                        assignmentRatingAverage={assignmentRatingAverage}
+                    />
                 </Route>
                 <Route path="/students/aranka">
-                    <Aranka />
+                    <Aranka
+                        assignmentRatingAverage={assignmentRatingAverage}
+                    />
                 </Route>
             </Switch>
         </main >
--- src/components/graphs/Bargraph.js
+++ src/components/graphs/Bargraph.js
@@ -408,73 +408,8 @@ const wincTheme = {
 };
 
 
-let assignmentRatingAverage = [
-    { assignment: "SCRUM" },
-    { assignment: "W1D1-1" },
-    { assignment: "W1D2-1" },
-    { assignment: "W1D2-2" },
-    { assignment: "W1D2-3" },
-    { assignment: "W1D2-4" },
-    { assignment: "W1D2-5" },
-    { assignment: "W1D3-1" },
-    { assignment: "W1D3-2" },
-    { assignment: "W1D3-4" },
-    { assignment: "W1D3-5" },
-    { assignment: "W1D3 - Project - Guess the number" },
-    { assignment: "W1D4-1" },
-    { assignment: "W1D4 - Project - Kleurentoggle" },
-    { assignment: "W1D5 - Project - Galgje" },
-    { assignment: "W2D1-1" },
-    { assignment: "W2D1-2" },
-    { assignment: "W2D2-1" },
-    { assignment: "W2D2-2" },
-    { assignment: "W2D2-3" },
-    { assignment: "W2D3-1" },
-    { assignment: "W2D3-2" },
-    { assignment: "W2D3-3" },
-    { assignment: "W2D4-1" },
-    { assignment: "W2D4-2" },
-    { assignment: "W2D4-3" },
-    { assignment: "W2D5 - Project - Filmzoeker" },
-    { assignment: "W3D1-1" },
-    { assignment: "W3D1-2" },
-    { assignment: "W3D1-3" },
-    { assignment: "W3D1-4" },
-    { assignment: "W3D2-1" },
-    { assignment: "W3D2-2" },
-    { assignment: "W3D2-3" },
-    { assignment: "W3D3-1" },
-    { assignment: "W3D3-2" },
-    { assignment: "W3D3-3" },
-    { assignment: "W3D3-4" },
-    { assignment: "W3D4-1" },
-    { assignment: "W3D4-2" },
-    { assignment: "W3D5 - Project - Todo-list" },
-    { assignment: "W4D2-1" },
-    { assignment: "W4D2-2" },
-    { assignment: "W4D2-3" },
-    { assignment: "W4D2-4" },
-    { assignment: "W4D3-1" },
-    { assignment: "W4D3-2" },
-    { assignment: "W4D3-3" },
-    { assignment: "W4D3-4" },
-    { assignment: "W4D3-5" },
-    { assignment: "W4D3 - Project - Next-level CSS" },
-    { assignment: "W5D4-1" },
-    { assignment: "W5D5 - Project - Lil_Playlist" },
-    { assignment: "W6D1-1" },
-    { assignment: "W6D2-1" },
-    { assignment: "W6D2 - Project - Eindopdracht" },
 
-];
-
-// const difficultyRating = () => {
-
-// }
-
-
-
-function Bargraph(studentData) {
+function Bargraph(studentData, assignmentRatingAverage) {
     const newStudentData = studentData.studentData.student;
     const assignmentData = newStudentData.map(x => x.assignments)
     const flattenArray = assignmentData.flat()
@@ -495,7 +430,7 @@ function Bargraph(studentData) {
     const assignmentsAverage = assignmentsTotal.map((current) => {
         return { name: current.name, difficultyRating: (current.difficultyRating / newStudentData.length), funRating: (current.funRating / newStudentData.length) }
     });
-    // console.log(assignmentsAverage)
+    console.log(assignmentsAverage)
     // bovenstaand heeft alle gemiddldes!!
 
     assignmentRatingAverage = assignmentsAverage.map(avg => ({
